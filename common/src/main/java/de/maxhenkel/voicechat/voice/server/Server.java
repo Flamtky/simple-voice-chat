7f843a483b8cd4a3590c8f97523fc33462511781
Finn Watermann <finn.watermann@fh-bielefeld.de>
2022-06-12 02:57:50 +0200

    Removed sysout

--- common/src/main/java/de/maxhenkel/voicechat/voice/server/Server.java
+++ common/src/main/java/de/maxhenkel/voicechat/voice/server/Server.java
@@ -231,7 +231,6 @@ public class Server extends Thread {
     }
 
     private void processMicPacket(ServerPlayer player, PlayerState state, MicPacket packet) throws Exception {
-        System.out.println("Processing mic packet from " + player.getName() + " toGroup: " + packet.toGroup + " whisper: " + packet.isWhispering());
         if (state.hasGroup() && packet.toGroup) {
             processGroupPacket(state, player, packet);
             if (Voicechat.SERVER_CONFIG.openGroups.get()) {
